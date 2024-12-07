088bff0974cdcb139a4653f10cab48178a619e72
husneara <husnearaa99@gmail.com>
2024-12-07 17:44:36 +0600

    has  ,  aggregation stage operator

---
+++ 6.1.js
@@ -0,0 +1,23 @@
+
+// $match , $project aggregation stage
+
+
+
+// $match operator
+db.test.aggregate ([
+    // stage-1
+    { $match: { gender: "Male"} }
+])
+
+
+
+// $match operator - method 2
+
+db.test.aggregate ([
+    // stage-1
+    { $match: { gender: "Male", age: { $lt: 30} } },
+    // stage-2
+    { $project: { name:1, gender:1 } }
+])
+
+
